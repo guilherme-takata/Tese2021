abb5b1d22aac7d7468c14a9963923419c634f1c5
Guilherme Takata <guilherme@j4s.tech>
2021-11-05 17:02:37 -0300

    New dataset treatment

--- Code/Online_retail.py
+++ Code/Online_retail.py
@@ -1,2 +1,23 @@
 import pandas as pd
-import plotly.graph_objects as go
+from IPython.display import display
+import datetime as dt
+
+dataframe_2009_2010 = pd.read_csv(r"C:\Users\GuilhermeTakata\Downloads\Kaggle_retail_DB\Year 2009-2010.csv", sep = ',', low_memory = False)
+
+dataframe_2010_2011 = pd.read_csv(r"C:\Users\GuilhermeTakata\Downloads\Kaggle_retail_DB\Year 2010-2011.csv", sep = ',', low_memory = False)
+
+display(dataframe_2009_2010)
+
+display(dataframe_2010_2011)
+
+dataframe_2009_2011 = pd.concat([dataframe_2009_2010, dataframe_2010_2011])
+
+display(dataframe_2009_2011)
+
+dataframe_2009_2011["InvoiceDate"] = pd.to_datetime(dataframe_2009_2011["InvoiceDate"])
+
+dataframe_2009_2011["InvoiceDate"] = dataframe_2009_2011["InvoiceDate"].dt.date
+
+display(dataframe_2009_2011)
+
+dataframe_2009_2011.to_csv(r"D:\Tese2021_datasets\Online_Retail.csv", sep = ',', encoding = 'utf-8', index = False)
